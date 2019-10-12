e34b8d3f3be4eb6ece790f7a283b888427225656
Nadezhda <npilshchikova@parseq.pro>
2019-10-12 22:29:32 +0300

    Updates

---
+++ precache-manifest.7b5eff875145910424a7c72fa119bf24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc7ca56d77108b538e55a59f376c485",
+    "url": "/syto-calc/index.html"
+  },
+  {
+    "revision": "3a804b4b3930c186384f",
+    "url": "/syto-calc/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "521cd10c75bcc10a3a3c",
+    "url": "/syto-calc/static/css/main.8c564f69.chunk.css"
+  },
+  {
+    "revision": "3a804b4b3930c186384f",
+    "url": "/syto-calc/static/js/2.53ce4b7e.chunk.js"
+  },
+  {
+    "revision": "521cd10c75bcc10a3a3c",
+    "url": "/syto-calc/static/js/main.799caf34.chunk.js"
+  },
+  {
+    "revision": "e3d4cbd2160d09475d05",
+    "url": "/syto-calc/static/js/runtime-main.1762d529.js"
+  }
+]);
